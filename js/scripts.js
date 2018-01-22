7e600a3c435d6dd4bec1d62612fd398d1ce35b35
Faiza Husain and Miranda Keith <miranda@>
2018-01-22 15:31:03 -0800

    Add remove button to tasks

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,66 @@
 // CONSTRUCTOR
-function Task (todo, dueDate) {
-  this.todo = todo;
+
+var tasks = [];
+var removedTasks = [];
+
+function Task (toDo, dueDate) {
+  this.todo = toDo;
   this.due = dueDate;
+  this.id = Task.prototype.id++;
+}
+
+Task.prototype.id = 0;
+
+Task.prototype.taskAndDate = function(){
+  return this.todo + " | " + this.due;
+}
+
+function removeTask(id){
+  //Determine the task that goes with the id
+  var task = tasks.filter(function(item){
+    return item.id === id;
+  })[0];
+  var taskIndex = tasks.indexOf(task);
+  if(taskIndex != -1){
+    tasks.splice(taskIndex, 1);
+  }
+  removedTasks.push(task);
+}
+
+//FRONT-END
+
+function displayTasks() {
+  $("#taskList ul").html("");
+  tasks.forEach(function(item){
+    $("#taskList ul").append('<li>'+item.taskAndDate() + '<button type="button" class="remove-task" taskid="' + item.id + '">Done</button></li>');
+    $("#taskList ul li:last-child .remove-task").click(function(){
+      var id = parseInt($(this).attr("taskid"));
+      removeTask(id);
+      displayTasks();
+    });
+  });
+  // $("#taskList ul").append("#taskList");
+  // $(".display-task ul:last-of-type").append("datesList");
 }
 
 
+$(document).ready(function () {
 
-//FRONT-END
+  $("form#toDoList").submit(function(event) {
+    event.preventDefault();
+
+    // alert("hi");
+    var taskName = $("#taskName").val();
+    var dueDate = $("#dueDate").val();
+
+    $("#taskName").val("");
+    $("#dueDate").val("");
+
+    var tasksToDo = new Task(taskName, dueDate);
+    tasks.push(tasksToDo);
+
+    displayTasks();
+
+  });
+
+});
